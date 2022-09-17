d2e4c52f57f0ed385556037d3971e595d796508a
SergeiB-web <sbury7596@gmail.com>
2022-09-17 08:40:04 +0300

    matricy

---
+++ main.py
@@ -0,0 +1,13 @@
+n = int(input("Введите кол-во строк:"))
+m = int(input("Введите кол-во столбцов:"))
+listC=[]
+for i in range(n):
+    listC1 = []
+    for j in range(m):
+        a = int(input())
+        listC1.append(a)
+    listC.append(listC1)
+for i in listC:
+    print(i)
+
+
